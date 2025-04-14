e92243ecefc3327214e7cbd133df97aca5f4f12d
saidevopseng <saidevopsengg@gmail.com>
2025-04-14 13:13:19 +0530

    base_URL_frontend

--- src/api.jsx
+++ src/api.jsx
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8000/api/',
+  // baseURL: 'http://localhost:8000/api/',
+  baseURL: 'https://sangamsoftsol-backend.onrender.com/api/',
 });
 
 export default api;
